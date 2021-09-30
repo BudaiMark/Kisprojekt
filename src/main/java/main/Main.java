c25ae216385b49b630a2795d82234650886b5f96
Budai Márk <budaimark00@gmail.com>
2021-09-30 20:05:01 +0200

    main metódus hozzáadva

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.application.Application;
+
+/**
+ * {@code main()} Ebben az osztályban kezdődik a játék lefutása {@code main()}
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(MyApplication.class, args);
+    }
+
+}
